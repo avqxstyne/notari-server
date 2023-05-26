9bbfb4467a1ae82339095cee0743beb29ae06ae2
August <augustwiesner16@gmail.com>
2023-05-26 14:36:57 -0500

    nothing

--- main.mjs
+++ main.mjs
@@ -12,18 +12,30 @@ var jsonParser = bodyParser.json()
 
 app.use(cors());
 
+// Setting up path for html file
+// app.use(express.static('public'));
+// app.get('*', (req, res) => {
+// 	console.log(req.params, req.body)
+//     res.sendFile(path.resolve(__dirname, 'public', 'index.html'));
+// });
 
-app.use(express.static('public'));
-app.get('*', (req, res) => {
-	console.log(req.params, req.body)
-    res.sendFile(path.resolve(__dirname, 'public', 'index.html'));
-});
+// Setting the port 
 app.listen(port, () =>
  	console.log(`Example app listening on port ${port}!`),
 );
 
+// Login post request reciever
 app.post('/login', jsonParser, (req, res) => {
 	console.log(req.body)
 	res.send("hello there gay")
 });
+
+// Homescreen - get all notes and turn into elements in sidebar
+app.get('/getsidebar', (req, res) => {
+	res.send({
+		note1: "Slef improvemnt",
+		note2: "scheudle",
+		note3: "DNA Minsdet"
+	})
+})
 
